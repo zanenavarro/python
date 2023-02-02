7503867b6d22e7117cc1912a0fb1b1c54b239b56
Zane Navarro <47710642+zanenavarro21@users.noreply.github.com>
2023-02-02 00:20:17 -0800

    updated rst script

--- scripts/update_rst.py
+++ scripts/update_rst.py
@@ -23,66 +23,40 @@ def main():
     version_name = file_info[1]
     folder_type = file_info[2]
     py_file = file_info[3]  
-
-    os.chdir(os.path.dirname(os.path.dirname(file_path)))
+    working_dir = os.getcwd()
+    #os.chdir(os.path.dirname(os.path.dirname(file_path)))
     print(os.popen("ls -l").read())
     #os.chdir("./docs")
     print(folder_type)
     if (folder_type == "lib"):
 
         rst_file = '{}.rst'.format(folder_name)
-        print(rst_file)
-        print(os.path.isfile(rst_file))
-        os.chdir(folder_name)
-        os.chdir(version_name)
-        os.chdir("docs")
-        # os.chdir('{}/{}/docs/'.format(folder_name, version_name))
-        # if (os.path.isfile(rst_file)):
         rst_str = ""
 
-        with open(file_path, 'r') as py_file:
-            py_contents = py_file.readlines()
-            py_file.close()
+        with open(file_path, 'r') as file_py:
+            py_contents = file_py.readlines()
+            file_py.close()
 
+        os.chdir("./{}/{}/docs/".format(folder_name, version_name))
         name_file = py_file.split(".")[0]
         for index, line in enumerate(py_contents):
-            print(line)
+            line_is_class = ""
             # regexp = re.compile(r'(class (.*):)')
-            line_is_class = re.search("(class (.*):)", line).group(2)
-            print(line_is_class)
+            try: 
+                line_is_class = re.search("(class (.*):)", line)
+            except:
+                pass
             if (line_is_class):
                 try:
-                    class_name = line_is_class.group(2)
+                    class_name = line_is_class.group(2).split('(')[0]
                     rst_str += ".. autoclass:: {0}.{1}\n  :members:\n".format(name_file, class_name)
                     print("adding {}".format(class_name))
                 except:
                     print("regex didnt matchh on line.")
-
+            # TODO : add support for auto generating functions if not class
         with open(rst_file, 'w') as read_file:
             read_file.write(rst_str)
             read_file.close()
-
-
-        
-def get_state(state, line):
-    comment_line = '"""'
-
-    if (comment_line in line):
-        count += 1
-        if (count == 1 and state == "SEARCHING"):
-            state = "SAMPLING"
-        elif (count == 2 and state == "SAMPLING"):
-            state = "SEARCHING"
-    
-
-
-
-    return state
-
-
-
-
-
 
 
 
